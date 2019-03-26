c507d3169f0b9ae87156984a4871989a0215f256
Tauqeer <tauqeer@grappetite.com>
2019-03-26 09:38:25 +0500

    Things

--- TofeeTV/ViewControllers/ThankYouViewController.m
+++ TofeeTV/ViewControllers/ThankYouViewController.m
@@ -60,6 +60,17 @@
 
 }
 
+-(void)viewDidAppear:(BOOL)animated
+{
+    
+    [super viewDidAppear:animated];
+    
+    [self.navigationController setNavigationBarHidden:NO animated:YES];
+    
+}
+
+
+
 -(void)goHome
 {
     
